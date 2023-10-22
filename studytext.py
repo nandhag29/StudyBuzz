cf73d6e9b1c7803513573cd9e4d20cd8eb516272
Chris <75055786+chrisiu@users.noreply.github.com>
2023-10-22 04:01:30 -0400

    New working

--- studytext.py
+++ studytext.py
@@ -1,10 +1,24 @@
+import os
 import whisper
 
-audio_file_path = input("Please enter the path to the audio file (e.g., audio.mp3): ")
 
+data_folder = 'backend/data'  # Assuming your videos are stored in a folder named 'data'
+
+# Get a list of all files in the data folder
+files = [os.path.join(data_folder, f) for f in os.listdir(data_folder) if os.path.isfile(os.path.join(data_folder, f))]
+
+# Sort files by modification time (newest first)
+files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+
+# Take the most recent file
+if files:
+    latest_video_path = files[0]
+else:
+    print("No video files found in the data folder.")
+    exit(1)
+
+# Use Whisper to transcribe the audio from the video
 model = whisper.load_model("base")
 
-result = model.transcribe(audio_file_path)
+result = model.transcribe(latest_video_path)
 print(result["text"])
-
-
